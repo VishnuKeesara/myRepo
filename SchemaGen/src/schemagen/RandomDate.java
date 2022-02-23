2b7fcd91fff53e4ea2e536c4f85120e3dcff7e8e
kvishu09 <100280400+kvishu09@users.noreply.github.com>
2022-02-23 15:55:14 +0530

    Add files via upload

---
+++ SchemaGen/src/schemagen/RandomDate.java
@@ -0,0 +1,25 @@
+
+package schemagen;
+
+
+import java.time.LocalDate;
+
+public class RandomDate {
+    public static void main(String[] args) {
+        
+            LocalDate randomDate = createRandomDate(1900, 2000);
+            System.out.println(randomDate);
+        
+    }
+
+    public static int createRandomIntBetween(int start, int end) {
+        return start + (int) Math.round(Math.random() * (end - start));
+    }
+
+    public static LocalDate createRandomDate(int startYear, int endYear) {
+        int day = createRandomIntBetween(1, 28);
+        int month = createRandomIntBetween(1, 12);
+        int year = createRandomIntBetween(startYear, endYear);
+        return LocalDate.of(year, month, day);
+    }
+}
